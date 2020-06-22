991a2398e222b8e3be686939c91a58d7f2dbff98
Nazmul <grey000906@gmail.com>
2020-06-23 05:53:13 +0600

    base app

--- src/store/index.js
+++ src/store/index.js
@@ -7,18 +7,18 @@ export default new Vuex.Store({
   state: {
     filter: 'all',
     todos: [
-      // {
-      //   id: 1,
-      //   title: 'Finish Vue Screencast',
-      //   completed: false,
-      //   editing: false
-      // },
-      // {
-      //   id: 2,
-      //   title: 'Take over world',
-      //   completed: false,
-      //   editing: false
-      // }
+      {
+        id: 1,
+        title: 'Task 01',
+        completed: false,
+        editing: false
+      },
+      {
+        id: 2,
+        title: 'Buy a pen',
+        completed: false,
+        editing: false
+      }
     ]
   },
   getters: {
