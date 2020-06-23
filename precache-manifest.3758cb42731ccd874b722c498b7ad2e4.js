e405e3df33e4656403acbabf0706f65e898b0405
Nazmul <grey000906@gmail.com>
2020-06-23 07:42:31 +0600

    deploy

---
+++ precache-manifest.3758cb42731ccd874b722c498b7ad2e4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb42c67829e3b4f9ccb3",
+    "url": "/vue-todo/css/app.bc172b76.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/vue-todo/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "72096788f716989defc824c0559068e3",
+    "url": "/vue-todo/index.html"
+  },
+  {
+    "revision": "bb42c67829e3b4f9ccb3",
+    "url": "/vue-todo/js/app.6d908d58.js"
+  },
+  {
+    "revision": "240bb3d9c4c81428c6fc",
+    "url": "/vue-todo/js/chunk-vendors.50be321f.js"
+  },
+  {
+    "revision": "fbafddbca0454e2cf83c4640f9391b35",
+    "url": "/vue-todo/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/vue-todo/robots.txt"
+  }
+]);
